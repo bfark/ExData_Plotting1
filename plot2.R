3ecfcd9c796f84b3c3b37bad71908f0142b8231f
bfark <barry_farkas@yahoo.com>
2014-05-11 16:09:49 -0400

    R code to product Plot 2

---
+++ plot2.R
@@ -0,0 +1,36 @@
+system.time(totData<-read.table("./household_power_consumption.txt", 
+                                header=TRUE, sep=";", stringsAsFactors=FALSE))
+
+
+projdata1<-totData[totData$Date=="1/2/2007",]
+projdata2<-totData[totData$Date=="2/2/2007",]
+
+projdata<-rbind(projdata1,projdata2)
+
+# Modified the dates to the correct format:
+projdata$Date<-as.Date(projdata$Date, "%d/%m/%Y")
+
+# Modify the times and paste the date to it for accuracy 
+projdata$Time<-strptime( paste(projdata$Date, projdata$Time), 
+                         "%Y-%m-%d %H:%M:%S")
+projdata$Time<-strptime( paste(projdata$Date, projdata$Time), 
+                         "%Y-%m-%d %H:%M:%S")
+
+
+# Now, screwing around with the histogram, I realize that the columns should
+# all be numeric:
+
+projdata$Global_active_power<-as.numeric(projdata$Global_active_power)
+projdata$Global_reactive_power<-as.numeric(projdata$Global_reactive_power)
+projdata$Voltage<-as.numeric(projdata$Voltage)
+projdata$Global_intensity<-as.numeric(projdata$Global_intensity)
+projdata$Sub_metering_1<-as.numeric(projdata$Sub_metering_1)
+projdata$Sub_metering_2<-as.numeric(projdata$Sub_metering_2)
+projdata$Sub_metering_3<-as.numeric(projdata$Sub_metering_3)
+
+
+# plot the time series of global active power and write it to a file:
+png("plot2.png")
+plot(ts(projdata$Global_active_power), ylab="Global Active Power (kilowatts)",xlab="", xaxt='n')
+axis(1, at = c(0, 1440, 2880), labels=c("Thu","Fri","Sat"))
+dev.off()

02c5ebe199474e61f71961f1366aa15f9b5ae218
bfark <barry_farkas@yahoo.com>
2014-05-11 16:33:38 -0400

    R code to produce Plot 3

---
+++ plot3.R
@@ -0,0 +1,39 @@
+system.time(totData<-read.table("./household_power_consumption.txt", 
+                                header=TRUE, sep=";", stringsAsFactors=FALSE))
+
+
+projdata1<-totData[totData$Date=="1/2/2007",]
+projdata2<-totData[totData$Date=="2/2/2007",]
+
+projdata<-rbind(projdata1,projdata2)
+
+# Modified the dates to the correct format:
+projdata$Date<-as.Date(projdata$Date, "%d/%m/%Y")
+
+# Modify the times and paste the date to it for accuracy 
+projdata$Time<-strptime( paste(projdata$Date, projdata$Time), 
+                         "%Y-%m-%d %H:%M:%S")
+projdata$Time<-strptime( paste(projdata$Date, projdata$Time), 
+                         "%Y-%m-%d %H:%M:%S")
+
+
+# Now, screwing around with the histogram, I realize that the columns should
+# all be numeric:
+
+projdata$Global_active_power<-as.numeric(projdata$Global_active_power)
+projdata$Global_reactive_power<-as.numeric(projdata$Global_reactive_power)
+projdata$Voltage<-as.numeric(projdata$Voltage)
+projdata$Global_intensity<-as.numeric(projdata$Global_intensity)
+projdata$Sub_metering_1<-as.numeric(projdata$Sub_metering_1)
+projdata$Sub_metering_2<-as.numeric(projdata$Sub_metering_2)
+projdata$Sub_metering_3<-as.numeric(projdata$Sub_metering_3)
+
+
+# plot the sub metering values as a time series write it to a file:
+png("plot3.png")
+plot(ts(projdata$Sub_metering_1), ylab="Energy sub metering",xlab="", xaxt='n')
+lines(ts(projdata$Sub_metering_2), col="red")
+lines(ts(projdata$Sub_metering_3), col="blue")
+axis(1, at = c(0, 1440, 2880), labels=c("Thu","Fri","Sat"))
+legend("topright",col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lwd=1)
+dev.off()
